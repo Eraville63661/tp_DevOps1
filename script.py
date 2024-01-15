80d523dab79b36b7a9b0bc6ed5a883874515f14a
Eraville63661 <45951775+Eraville63661@users.noreply.github.com>
2024-01-15 14:24:18 +0100

    6er Push

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
 prenom = 'Ryan'
 matiere = 'Cours de DevOps'
 
-print('Programme de :' - str(prenom) - ' en cours de :' - str(matiere))
+print('Programme de :' + str(prenom) + ' en cours de :' + str(matiere))
