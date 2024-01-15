fd70700263bed8eb191e201041cea68290ef31ae
Eraville63661 <45951775+Eraville63661@users.noreply.github.com>
2024-01-15 15:44:44 +0100

    test fonction

--- script.py
+++ script.py
@@ -1,4 +1,13 @@
-prenom = 'Ryan'
-matiere = 'Cours de DevOps'
+def ecrit(prenom, matiere):
+    if len(prenom) == 0:
+        prenom = 'Ryan'
+    if len(matiere) == 0 :
+        matiere = 'DevOps'
 
-print('Programme de :' + str(prenom) + ' en cours de :' + str(matiere))
+    print('Programme de : ' + str(prenom) + '\nEn cours de : ' + str(matiere))
+
+
+# if __name__ == "__main__":
+#     prenom = ''
+#     matiere = ''
+#     ecrit(prenom, matiere)
